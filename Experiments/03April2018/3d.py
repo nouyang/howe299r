c6ab05d4c5b2fbdf1aa8d84f63889e00b531e345
nouyang <nouyang@email>
2018-04-10 04:10:45 -0400

    using sklearn to predict 3 values of K for F_z

--- Experiments/03April2018/3d.py
+++ Experiments/03April2018/3d.py
@@ -27,7 +27,7 @@ BigTorque = np.zeros((1,3))
 #### DECLARE CONSTANTS ####
 #===============================================
 listpos = range(15)
-listpos = [14]
+listpos = [12]
 #listpos = range(1,3)
 xs = [4.6, 4.1, 3.5, 3.1, 2.6]#pos 1 x = 4.6 cm
 #ys = [0.4, 0.1, -0.2]
@@ -46,6 +46,8 @@ for i in listpos:
     imuDat = pd.read_csv(path+fname, header=None,sep=';', 
                         names = IMUCols, skip_blank_lines=True, usecols=[0,1,2,3,4]) 
     imuDat = imuDat.drop(['timeSysCal', 'XYZ'], 1)
+    #print(imuDat.describe())
+    #print(imuDat.head())
     bkgd = imuDat.iloc[0::2]  
     signal = imuDat.iloc[1::2]
     zer = bkgd.as_matrix()
@@ -72,38 +74,70 @@ for i in listpos:
     #print(forces)
 
     torques = np.cross(pos.reshape(-1,1).T, forces) #n.3
-    print('3D torques:\n', torques)
-    print('3D deflections: \n', thetas)
+    #print('3D torques:\n', torques)
+    #print('3D deflections: \n', thetas)
     #torquesX, torquesY, torquesZ = torques[:,0] , torques[:,1], torques[:,2]
     BigTheta = np.vstack((BigTheta, thetas))
     BigTorque = np.vstack((BigTorque, torques))
 
 BigTheta = BigTheta[1:,:] #remove first row of zeros, from init
 BigTorque = BigTorque[1:,:]
-
+print(BigTheta.shape) #n.3
+print(BigTorque.shape) #n.3
 
 print('number of datapoints', BigTheta.shape)
 
 #### CALCULATE K ####
-matK = np.linalg.lstsq(BigTorque, BigTheta, rcond=None)[0]
+matK = np.linalg.lstsq(BigTheta, BigTorque, rcond=None)[0]
+
 print(matK.shape)
 print(matK)
 print('position number: ', i+1)
 
+#===============================================
+#### PLOT ####
+#===============================================
+
+# let us retrieve estimates of K via sklearn library to check numpy.lstsq, which gives us all zeros
+# for the third row of K :(
+
+names = ['x:roll', 'y:pitch', 'z:yaw']
+dim = 1
+torq_1D = BigTorque[:,dim] #z col
+#theta_1D = BigTheta[:,dim]
+print('torq1d shape', torq_1D.shape)
+
+myX = BigTheta#theta_1Dreshape(-1,1)
+myy = torq_1D 
+
+#regr= Ridge(fit_intercept=True, alpha=1.0, random_state=0, normalize=True)
+regr = linear_model.LinearRegression()
+regr.fit(myX, myy)
+coef_ridge = regr.coef_
+#gridx = np.linspace(myX.min(), myX.max(), 20)
+#coef_ = regr.coef_ * gridx + regr.intercept_
+yPred= regr.predict(myX) 
+
+print('K Coefficients: \n', regr.coef_)
+print("Mean squared error: %.2f" % mean_squared_error(torq_1D, yPred)) 
+#print('Variance score (ideal 1): %.2f' % r2_score(thetaY))
+
+# http://stackabuse.com/linear-regression-in-python-with-scikit-learn/
 '''
 #===============================================
 #### PLOT ####
 #===============================================
 
+# let us retrieve estimates of K via sklearn library to check numpy.lstsq, which gives us all zeros
+# for the third row of K :(
+
 ## Note: For the IMU, orientation.Y is pitch; X is roll; Z is yaw
 
-torquesY = BigTorque
-thetaY = BigTheta
-trace0 = go.Scatter( x = thetaY , y = torquesY, mode = 'markers',
-    name = 'degrees (by IMU)' )
+trace0 = go.Scatter( x = torq_1D, y = theta_1D, mode = 'markers',
+    name = names[dim] + ' deflection in degrees (by IMU)' )
 
-myX = thetaY.reshape(-1,1)
-myy = torquesY
+myX = theta_1D.reshape(-1,1)
+myy = torq_1D 
 
 #regr= Ridge(fit_intercept=True, alpha=1.0, random_state=0, normalize=True)
 regr = linear_model.LinearRegression()
@@ -111,8 +145,8 @@ regr.fit(myX, myy)
 coef_ridge = regr.coef_
 gridx = np.linspace(myX.min(), myX.max(), 20)
 coef_ = regr.coef_ * gridx + regr.intercept_
-print(regr.coef_)
-print(regr.intercept_)
+#print(regr.coef_)
+#print(regr.intercept_)
 yPred= regr.predict(myX) 
 #plt.plot(gridx, coef_, 'g-', label="ridge regression")
 
@@ -122,15 +156,15 @@ trace2 = go.Scatter( x= gridx, y = coef_,
 data = [trace0, trace2]
     
 layout = go.Layout(
-    title='Pitch: degrees of deflection vs torque',
+    title= names[dim] + ' degrees of deflection vs torque',
     yaxis=dict(title='torque (in grams)'),
     xaxis=dict(title='degrees'),
     legend=dict(x=.1, y=-.5) )
 
 fig = go.Figure(data=data, layout=layout)
 
-print('Coefficients: \n', regr.coef_)
-print("Mean squared error: %.2f" % mean_squared_error(thetaY, yPred))
+print('K Coefficients: \n', regr.coef_)
+print("Mean squared error: %.2f" % mean_squared_error(theta_1D, yPred)) 
 #print('Variance score (ideal 1): %.2f' % r2_score(thetaY))
 po.plot(fig)
 
